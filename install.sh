7c425697ecd9f3a7f61d1a912a8ec41ae95bf59d
jamestan <jamestan@tencent.com>
2019-09-14 13:07:21 +0800

    up

---
+++ install.sh
@@ -0,0 +1,13 @@
+# add by Chuanqi Tan
+
+
+# git clone plugins
+PLUGINS_DIR="./my_plugins"
+
+git clone https://github.com/majutsushi/tagbar $PLUGINS_DIR
+git clone https://github.com/davidhalter/jedi-vim $PLUGINS_DIR
+git clone https://github.com/scrooloose/nerdcommenter $PLUGINS_DIR
+
+
+# install
+sh install_awesome_vimrc.sh
--- my_configs.vim
+++ my_configs.vim
@@ -1,2 +1,11 @@
 set nu
 let g:loaded_comfortable_motion = 0
+
+nmap <F8> :TagbarToggle<CR>
+
+nmap <F1> :NERDTreeToggle<CR>
+let g:NERDTreeWinPos = "left"
+
+
+
+
