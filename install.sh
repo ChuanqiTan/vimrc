601d27335ae9c43b2f43de53b2afecf8c6fb29bf
tanchuanqi <chuanqi.tan@gmail.com>
2019-09-15 08:55:17 +0800

    up sh

--- install.sh
+++ install.sh
@@ -1,13 +1,17 @@
 # add by Chuanqi Tan
 
 
-# git clone plugins
+SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
 PLUGINS_DIR="./my_plugins"
+echo $SHELL_FOLDER
 
-git clone https://github.com/majutsushi/tagbar $PLUGINS_DIR
-git clone https://github.com/davidhalter/jedi-vim $PLUGINS_DIR
-git clone https://github.com/scrooloose/nerdcommenter $PLUGINS_DIR
+
+# git clone plugins
+git clone https://github.com/majutsushi/tagbar $PLUGINS_DIR/tagbar
+git clone https://github.com/davidhalter/jedi-vim $PLUGINS_DIR/jedi-vim
+git clone https://github.com/scrooloose/nerdcommenter $PLUGINS_DIR/nerdcommenter
 
 
 # install
+ln -sf $SHELL_FOLDER ~/.vim_runtime
 sh install_awesome_vimrc.sh
