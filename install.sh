3ddfeb377881c4cbd85cee82166971e3cfae3a03
Chuanqi <chuanqi.tan@gmail.com>
2022-11-25 22:11:40 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 # add by Chuanqi Tan
 
 
+# install jedi package, for jedi-vim use
+echo "sudo apt install -y python3-pip && pip install jedi"  
+
+
 SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
 PLUGINS_DIR="./my_plugins"
 echo $SHELL_FOLDER
@@ -16,4 +20,5 @@ git clone https://github.com/scrooloose/nerdcommenter $PLUGINS_DIR/nerdcommenter
 ln -sf $SHELL_FOLDER ~/.vim_runtime
 sh install_awesome_vimrc.sh
 
-echo "run :PlugInstall & PlugUpdate in VIM"
+
+vim +PlugInstall
